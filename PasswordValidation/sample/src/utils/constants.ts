778e648147b919ab7f60999dbdcbf9aec1c686a3
AvinashMattupalli03 <avinash.mattupalli@zemosolabs.com>
2024-07-11 19:01:51 +0530

    Password Validation Task

---
+++ PasswordValidation/sample/src/utils/constants.ts
@@ -0,0 +1,18 @@
+export const API_URL = "https://jsonplaceholder.typicode.com";
+export const HEADING = "Last 10 Posts By Using Query Parameter";
+
+export const NAMES: string[] = [
+  "Avinash",
+  "Pavan",
+  "Narendra",
+  "Ajay",
+  "Bharath",
+  "Shravya",
+  "Jameela",
+];
+
+export const PASSWORD_RULES =
+  "Must contain at least one uppercase letter, one special character,lowercase letters, and length between 3 to 10 characters";
+
+export const TITLE = "Password Validation";
+export const BUTTON_LABEL = "Submit";
